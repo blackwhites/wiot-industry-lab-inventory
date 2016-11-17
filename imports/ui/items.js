29c2ca1e1f93f48e82d24ff3584881c7703dc7c1
Tobias Stoeckel <tobias.stoeckel@de.ibm.com>
2016-11-17 11:26:50 +0100

    Enabled item deletion

--- imports/ui/items.js
+++ imports/ui/items.js
@@ -1,1 +1,16 @@
+import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
+
 import './items.html';
+
+import { Items } from '../api/items.js'
+
+Template.item.onCreated(function itemOnCreated() {
+    this.state = new ReactiveDict();
+});
+
+Template.item.events({
+    'click .delete'() {
+        Items.remove(this._id);
+    },
+});
